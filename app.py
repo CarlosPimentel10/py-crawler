003e4a5fc6de6e53ee0819bc67761bd3bff61bd0
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2019-11-14 17:00:26 +0100

    Commiting python web scraper

---
+++ app.py
@@ -0,0 +1,13 @@
+# Web scrapper code
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://stackoverflow.com/questions")
+soup = BeautifulSoup(response.text, "html.parser")
+
+questions = soup.select(".question-summary")
+for question in questions:
+    print(question.select_one(".question-hyperlink").getText)
+    print(question.select_one(".vote-count-post").getText)
+
+
